dc9da2d4462f3b156581ca3f3b41f2d2d4ee38d2
Petar <pecadeljanin@gmail.com>
2024-06-13 00:03:14 +0200

    Refactor file names. Add theme switching.

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,8 @@ import { Input } from '@/components/ui/input';
 import Link from 'next/link';
 import { Separator } from '@/components/ui/separator';
 import DynamicBreadcrumb from '@/components/dynamicBreadcrumb';
+import { ThemeProvider } from '@/components/theme-provider';
+import { ThemeSwitcher } from '@/components/theme-switcher';
 let mockTopics = [
   { id: 1, name: 'Tech', icon: Code, href: '#' },
   { id: 2, name: 'Game of life', icon: Waypoints, href: '#' },
@@ -110,78 +112,31 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <body className={inter.className}>
-        <div className="fixed inset-0  grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-          <div className=" hidden border-r bg-muted/40 md:block">
-            <div className="flex h-full max-h-screen flex-col gap-2">
-              <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                <Link
-                  href="/"
-                  className="flex items-center gap-2 font-semibold">
-                  <Home className="h-6 w-6" />
-                  <span className="">Da blog</span>
-                </Link>
-                {/* <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          // disableTransitionOnChange
+        >
+          <div className="fixed inset-0  grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <div className=" hidden border-r bg-muted/40 md:block">
+              <div className="flex h-full max-h-screen flex-col gap-2">
+                <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+                  <Link
+                    href="/"
+                    className="flex items-center gap-2 font-semibold">
+                    <Home className="h-6 w-6" />
+                    <span className="">Da blog</span>
+                  </Link>
+                  {/* <Button variant="outline" size="icon" className="ml-auto h-8 w-8">
               <Bell className="h-4 w-4" />
               <span className="sr-only">Toggle notifications</span>
             </Button> */}
-              </div>
-              <div className="flex-1">
-                <nav className="flex flex-col px-2 gap-3 text-sm font-medium lg:px-4">
-                  {mockTopics.map((topic) => (
-                    <>
-                      <div>
-                        <Link
-                          key={topic.id}
-                          href={topic.href}
-                          className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
-                          <topic.icon className="h-4 w-4" />
-                          {topic.name}
-                        </Link>
-                        <Separator />
-
-                        <ScrollArea className="ml-2 py-1 h-64 w-full rounded-lg ">
-                          {mockPosts.map((post) => (
-                            <>
-                              <Link
-                                key={post.id}
-                                href={post.href}
-                                className="flex ml-4 py-2 text-muted-foreground transition-all hover:text-primary">
-                                <span className="">{post.title}</span>
-                              </Link>
-                            </>
-                          ))}
-                        </ScrollArea>
-                      </div>
-                    </>
-                  ))}
-                </nav>
-              </div>
-              <div className="mt-auto p-4">
-                <Link
-                  href="#"
-                  className="flex items-center gap-2 px-2 w-full text-lg font-semibold text-muted-foreground md:text-base">
-                  <Settings className="h-5 w-5 transition-all " />
-                  <span className="">Settings</span>
-                </Link>
-              </div>
-            </div>
-          </div>
-          <div className="flex flex-col overflow-y-auto">
-            <header className="flex h-14 flex-shrink-0 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button
-                    variant="outline"
-                    size="icon"
-                    className="shrink-0 md:hidden">
-                    <Menu className="h-5 w-5" />
-                    <span className="sr-only">Toggle navigation menu</span>
-                  </Button>
-                </SheetTrigger>
-                <SheetContent side="left" className="flex flex-col">
-                  <nav className="grid gap-2 text-lg font-medium">
+                </div>
+                <div className="flex-1">
+                  <nav className="flex flex-col px-2 gap-3 text-sm font-medium lg:px-4">
                     {mockTopics.map((topic) => (
                       <>
                         <div>
@@ -210,43 +165,98 @@ export default function RootLayout({
                       </>
                     ))}
                   </nav>
-                </SheetContent>
-              </Sheet>
-              <div className="w-full flex-1">
-                <form>
-                  <div className="relative">
-                    <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                    <Input
-                      type="search"
-                      placeholder="Search products..."
-                      className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
-                    />
-                  </div>
-                </form>
+                </div>
+                <div className="mt-auto p-4">
+                  <Link
+                    href="#"
+                    className="flex items-center gap-2 px-2 w-full text-lg font-semibold text-muted-foreground md:text-base">
+                    <Settings className="h-5 w-5 transition-all " />
+                    <span className="">Settings</span>
+                  </Link>
+                </div>
               </div>
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="secondary"
-                    size="icon"
-                    className="rounded-full">
-                    <CircleUser className="h-5 w-5" />
-                    <span className="sr-only">Toggle user menu</span>
-                  </Button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent align="end">
-                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>Settings</DropdownMenuItem>
-                  <DropdownMenuItem>Support</DropdownMenuItem>
-                  <DropdownMenuSeparator />
-                  <DropdownMenuItem>Logout</DropdownMenuItem>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </header>
-            {children}
+            </div>
+            <div className="flex flex-col overflow-y-auto">
+              <header className="flex h-14 flex-shrink-0 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button
+                      variant="outline"
+                      size="icon"
+                      className="shrink-0 md:hidden">
+                      <Menu className="h-5 w-5" />
+                      <span className="sr-only">Toggle navigation menu</span>
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent side="left" className="flex flex-col">
+                    <nav className="grid gap-2 text-lg font-medium">
+                      {mockTopics.map((topic) => (
+                        <>
+                          <div>
+                            <Link
+                              key={topic.id}
+                              href={topic.href}
+                              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary">
+                              <topic.icon className="h-4 w-4" />
+                              {topic.name}
+                            </Link>
+                            <Separator />
+
+                            <ScrollArea className="ml-2 py-1 h-64 w-full rounded-lg ">
+                              {mockPosts.map((post) => (
+                                <>
+                                  <Link
+                                    key={post.id}
+                                    href={post.href}
+                                    className="flex ml-4 py-2 text-muted-foreground transition-all hover:text-primary">
+                                    <span className="">{post.title}</span>
+                                  </Link>
+                                </>
+                              ))}
+                            </ScrollArea>
+                          </div>
+                        </>
+                      ))}
+                    </nav>
+                  </SheetContent>
+                </Sheet>
+                <div className="w-full flex-1">
+                  <form>
+                    <div className="relative">
+                      <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                      <Input
+                        type="search"
+                        placeholder="Search products..."
+                        className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
+                      />
+                    </div>
+                  </form>
+                </div>
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <Button
+                      variant="secondary"
+                      size="icon"
+                      className="rounded-full">
+                      <CircleUser className="h-5 w-5" />
+                      <span className="sr-only">Toggle user menu</span>
+                    </Button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent align="end">
+                    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Settings</DropdownMenuItem>
+                    <DropdownMenuItem>Support</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem>Logout</DropdownMenuItem>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+                <ThemeSwitcher />
+              </header>
+              {children}
+            </div>
           </div>
-        </div>
+        </ThemeProvider>
       </body>
     </html>
   );
